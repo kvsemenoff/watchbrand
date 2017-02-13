79108e42776297e7afebc133ffa8e31df18f418d
unknown <anzarsh@mail.ru>
2017-02-13 13:53:26 +0300

    add 1

--- js/common.js
+++ js/common.js
@@ -1,6 +1,29 @@
 
 
 $(document).ready(function(){
+  var owl = $(".slider-db");
+
+owl.owlCarousel({
+
+        loop:true,//Зацикливаем слайдер
+        nav:true, //Навигация включена
+        autoplay:false,//автозапуск
+        smartSpeed:1000,//Время движения
+        margin:0,    
+        navText:['<span class="db-left"></span>','<span class="db-right"></span>'],
+        responsive:{
+          0:{
+            items:1
+        },       
+        1000:{
+            items:1
+        },
+        1248:{
+            items:1
+        }
+    }
+
+});
 
     $('.az-select').each(function(){
         var select = $(this);    
