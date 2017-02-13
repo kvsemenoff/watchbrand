0d2033544e169dffd3f5514d20b993aeb4a4aee2
Berzeg07 <berezg86@gmail.com>
2017-02-13 11:06:19 +0300

    29.14

--- tech.php
+++ tech.php
@@ -4,12 +4,13 @@
 
 
 	<form class="common-form" action="#">
-		<input type="text" value="" placeholder="Ваше имя">	
-		<input type="text value="" placeholder="Ваш телефон">		
+		<input type="text" name="uname" value="" placeholder="Ваше имя">	
+		<input type="text" class="phone" name="phone" value="" placeholder="Ваш телефон">		
 		<input type="submit" value="Заказать сейчас" placeholder="">		
 	</form>		
-
-
+<br><br>
+<strong>желтый цвет в тексте через тег strong</strong>
+<br><br>
 </section>
 black-bg
 
